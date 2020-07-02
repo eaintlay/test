6ed2a5dc44cf01370311865ca9c3b2c753b8a5fd
eaintlay <haythi315811@gmail.com>
2020-07-02 17:45:28 +0630

    message

---
+++ Exercise11.php
@@ -0,0 +1,14 @@
+<?php  
+$dolar1 = "1000$";
+$dolar2 = "1200$";
+$dolar3 = "1400$";
+
+
+
+
+echo ' Salary of Mr.A is '.$dolar1. '<br>';
+echo ' Salary of Mr.B is '.$dolar2. '<br>';
+echo ' Salary of Mr.C is '.$dolar3. '<br>';
+
+ ?>
+
